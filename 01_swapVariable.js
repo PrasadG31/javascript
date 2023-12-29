3a468cd7fd13116f30500eb58c4583c81a17f6a1
PrasadG31 <prasadghorpade11@gmail.com>
2023-12-29 17:15:33 +0530

    swap variable
    swap variable

---
+++ 01_swapVariable.js
@@ -0,0 +1,16 @@
+
+
+var n1="10";
+var n2="20";
+
+console.log("Before Swap");
+console.log("N1 :",n1);
+console.log("N2 :",n2);
+
+var temp=n1;
+n1=n2;
+n2=temp;
+
+console.log("After Swap");
+console.log("N1:",n1);
+console.log("N2:",n2);
