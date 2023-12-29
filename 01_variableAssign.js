809b61343eeafd3415139f937674b054e0078f4a
PrasadG31 <prasadghorpade11@gmail.com>
2023-12-29 16:55:33 +0530

    basic variable added
    basic variable added

---
+++ 01_variableAssign.js
@@ -0,0 +1,25 @@
+console.log("===== My Full Name =====");
+
+var myFullName;
+console.log(myFullName);
+
+myFullName="Prasad Ghorpade"
+console.log("Full Name:", myFullName);
+
+console.log("===== My Love =====");
+
+var myLove="I love only JS"
+console.log(myLove);
+
+console.log("=====Same line=====");
+
+var firstName="Prasad";
+var lastName="Ghorpade";
+var age="28";
+var address="Pune";
+var pinCode="411062";
+var gender="Male";
+var isMarried="Yes";
+
+console.log("First Name:",firstName,",","Last Name:",lastName,",","Age:",age,",","Address:",address,",","Pincode:",pinCode,",","Gender:",gender,",","Married:",isMarried,".");
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
 console.log("Hello JS, I am Prasad");
+
+
+var myName;  //Variable declaration
+console.log(myName);  //undefined
+
+myName="Prasad";  //variable initialization
+console.log("First Name:",myName);
+
+var age="28";  //variable declaration and initialization
+console.log("Age:", age);
+
+myName="Prasad Ghorpade"; //variable update
+console.log(myName);
