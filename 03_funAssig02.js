b0a75a8266bc85d863d4d8e5ff02030219513b40
PrasadG31 <prasadghorpade11@gmail.com>
2023-12-30 13:39:09 +0530

    Funassign02
    Funassign02

---
+++ 03_funAssig02.js
@@ -0,0 +1,6 @@
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank Name;",bankName,"-", "Account Num:",accountNum, "-","location",location, "-","PinCode",pinCode );
+}
+bankDetails('SBI Bank',112233,'Pune',1234);
+bankDetails('HDFC Bank',223343,'Mumbai',2345);
+bankDetails('ICICI Bank',777999,'Satara',631207);
